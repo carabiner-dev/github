442fdacc1d16783551657ea1a6d9815d26c84d56
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-02-10 16:22:26 -0600

    Add default token reader
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- tokenreader.go
+++ tokenreader.go
@@ -8,14 +8,24 @@ import (
 	"os"
 )
 
+// DefaultEnvTokenReader is an env token reader configured to read the token
+// from the GITHUB_TOKEN env variable.
+var DefaultEnvTokenReader = EnvTokenReader{
+	VarName: "GITHUB_TOKEN",
+}
+
+// TokenReader is an interface that abstracts a mechanism to read a token securely.
 type TokenReader interface {
 	ReadToken() (string, error)
 }
 
+// EnvTokenReader is a token reader that extracts an API token from an environment
+// variable
 type EnvTokenReader struct {
 	VarName string
 }
 
+// ReadToken reads the token string from the convigured `VarName` environment variable.
 func (etr *EnvTokenReader) ReadToken() (string, error) {
 	if etr.VarName == "" {
 		return "", errors.New("environment variable name to read token not set")
