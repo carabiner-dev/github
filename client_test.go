fdefe59b3b35f8c6f133b151f174869d636ab4b6
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-02-26 20:12:46 -0600

    Add test for client.Call
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

---
+++ client_test.go
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package github
+
+import (
+	"context"
+	"errors"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCall(t *testing.T) {
+	t.Parallel()
+	for _, tc := range []struct {
+		name     string
+		data     []byte
+		synthErr error
+	}{
+		{"normal", []byte("Hi\n"), nil},
+		{"normal", nil, errors.New("Superbad HTTP Error!")},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Initialize the caller with the file caller to test
+			c, err := NewClient(
+				WithCaller(&FileCaller{
+					SourcePath: "testdata/file.txt",
+					Error:      tc.synthErr,
+				}),
+			)
+			require.NoError(t, err)
+
+			res, err := c.Call(context.Background(), http.MethodGet, "http://example.com/", nil)
+			if tc.synthErr != nil {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			defer res.Body.Close()
+
+			data, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+			require.Equal(t, tc.data, data)
+		})
+	}
+}
